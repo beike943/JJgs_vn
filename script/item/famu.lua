45af9e0fe4a1349c19c727db969ba71adb0b37f0
gzsklsskszngc <60936777+gzsklsskszngc@users.noreply.github.com>
2020-02-16 23:21:17 +0800

    Update famu.lua

--- script/item/famu.lua
+++ script/item/famu.lua
@@ -1,4 +1,4 @@
---·¥Ä¾
+--ä¼æœ¨
 Include("\\script\\function\\zq_battles\\zq_head.lua")
 
 function OnUse(nItem)
@@ -6,6 +6,6 @@ function OnUse(nItem)
 	if n == 710 or n == 715 or n == 720 then
 		OpenNewGatherWindow()
 	else
-		Msg2Player("Kh«ng thÓ sö dông c«ng cô thu thËp nµy ë ®©y.")
+		Msg2Player("ä½ ä¸èƒ½åœ¨è¿™é‡Œä½¿ç”¨è¿™ä¸ªé‡‡é›†å·¥å…·")
 	end
 end
