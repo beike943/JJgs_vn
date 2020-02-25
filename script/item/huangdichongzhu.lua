2316657ffef7c14d92adb5b0ff428415095012d8
gzsklsskszngc <60936777+gzsklsskszngc@users.noreply.github.com>
2020-02-25 17:13:31 +0800

    更新黄帝重铸图纸--完美汉化！

--- script/item/huangdichongzhu.lua
+++ script/item/huangdichongzhu.lua
@@ -1,23 +1,23 @@
---½Å±¾Ãû³Æ£º»ÆµÛÖØÔìÍ¼Ö½Ê¹ÓÃ½Å±¾
---½Å±¾¹¦ÄÜ£º¸øÍæ¼ÒÏÔÊ¾ÖØÖıĞèÒªµÄ²ÄÁÏ
---²ß»®ÈË£ºĞ¡ÌìÌì
---´úÂë¿ª·¢ÈË£º´å³¤
---´úÂë¿ª·¢Ê±¼ä£º2007-07-02
+--è„šæœ¬åç§°ï¼šé»„å¸é‡é€ å›¾çº¸ä½¿ç”¨è„šæœ¬
+--è„šæœ¬åŠŸèƒ½ï¼šç»™ç©å®¶æ˜¾ç¤ºé‡é“¸éœ€è¦çš„ææ–™
+--ç­–åˆ’äººï¼šå°å¤©å¤©
+--ä»£ç å¼€å‘äººï¼šæ‘é•¿
+--ä»£ç å¼€å‘æ—¶é—´ï¼š2007-07-02
 Tb_refound_mertiral = {
-	{2,1,504,4,"N÷ Oa Tinh Th¹ch"},
-	{2,0,555,2,"Cµn Kh«n phï"},
-	{2,1,503,5,"V« NgÊn Thñy"},
-	{2,1,1009,200,"Thiªn Th¹ch Tinh Th¹ch"},
+	{2,1,504,4,"å¥³å¨²ç²¾çŸ³"},
+	{2,0,555,2,"ä¹¾å¤ç¬¦"},
+	{2,1,503,5,"æ— ç—•æ°´"},
+	{2,1,1009,200,"é™¨é“ç²¾çŸ³"},
 }
 function OnUse()
-	local mertiral_need_dia = "<color=green>Hoµng ®Õ trïng t¹o ®å<color>: Theo ®å chØ b¹n sÏ biÕt <color=red>Bé Hoµng ®Õ<color> cÇn nh÷ng nguyªn liÖu sau"
+	local mertiral_need_dia = "<color=green>é»„å¸é‡é“¸å›¾çº¸<color>:æ®æˆ‘æ‰€çŸ¥<color=red>é‡é“¸é»„å¸å¥—è£…<color>éœ€è¦ä»¥ä¸‹ææ–™"
 	for i = 1, getn(Tb_refound_mertiral) do
 		local num_color = "<color=red>"
 		local mertiral_num = GetItemCount(Tb_refound_mertiral[i][1],Tb_refound_mertiral[i][2],Tb_refound_mertiral[i][3])
 		if mertiral_num >= Tb_refound_mertiral[i][4] then
 			num_color = "<color=green>"
 		end
-		mertiral_need_dia = mertiral_need_dia .."\n    <color=yellow>"..Tb_refound_mertiral[i][5]..Tb_refound_mertiral[i][4].."<color>, hiÖn b¹n cã"..num_color..mertiral_num.."<color>;"
+		mertiral_need_dia = mertiral_need_dia .."\n    <color=yellow>"..Tb_refound_mertiral[i][5]..Tb_refound_mertiral[i][4].."<color>,æ‚¨å½“å‰å·²æœ‰"..num_color..mertiral_num.."<color>;"
 	end
 	Talk(1,"",mertiral_need_dia)
 end
