bcd75fa746605f6d87dc730fdb3493614ff153ff
gzsklsskszngc <60936777+gzsklsskszngc@users.noreply.github.com>
2020-03-02 17:55:03 +0800

    Update jingyuye.lua

--- script/item/jingyuye.lua
+++ script/item/jingyuye.lua
@@ -2,11 +2,11 @@ Include("\\script\\equip_shop\\equip_shop_head.lua")
 
 function OnUse(nItem)
 	local tSay = {
-		format("TiÖm S¸ch Kü N¨ng CÊp 1-2/#show_equip_shop(%d)", 3047),
-		format("TiÖm S¸ch Kü N¨ng CÊp 3-4/#show_equip_shop(%d)", 3048),
-		"Hñy bá/nothing",	
+		format("1-2çº§æŠ€èƒ½å•†åº—/#show_equip_shop(%d)", 3047),
+		format("3-4çº§æŠ€èƒ½å•†åº—#show_equip_shop(%d)", 3048),
+		"å–æ¶ˆ/nothing",	
 	}
-	Say("H·y chän tiÖm cÇn ®æi: ", getn(tSay), tSay);
+	Say("é€‰æ‹©æ‚¨è¦æ›´æ¢çš„å•†åº—: ", getn(tSay), tSay);
 end
 
 function nothing()
